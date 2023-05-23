929368f633a45055e869a3806b088a21fd4a2819
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2023-05-23 11:18:34 +0300

    updates

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import Error from '../components/Error/Error';
-const NotFound = () => {
+const GenericNotFound = () => {
   return <Error />;
 };
-export default NotFound;
+export default GenericNotFound;
