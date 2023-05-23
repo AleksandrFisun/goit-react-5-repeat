fd111c76b86f1fad13c706fc0ecc831f7c5dadf9
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2023-05-23 11:02:17 +0300

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../pages/Home'));
@@ -22,8 +22,15 @@ export const App = () => {
           <Route path="search/:id" element={<FilmDetails />}>
             <Route path="more" element={<AdditionalInformation />}></Route>
           </Route>
-          <Route path="*" element={<NotFound />} />
         </Route>
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<h2>Loading ...</h2>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Routes>
     </>
   );
