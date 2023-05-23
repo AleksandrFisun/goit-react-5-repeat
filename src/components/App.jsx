685e52216c1f9f0bedf3132505bf6da546d6d893
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2023-05-23 12:40:30 +0300

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,57 @@
 import { Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
-import SharedLayout from './SharedLayout/SharedLayout';
+// import { lazy } from 'react';
+import {
+  Searchbar,
+  Link,
+  Nav,
+  NavList,
+  NavItem,
+  Logotype,
+  LogotypeWrapper,
+  LogotypeText,
+} from './Global.style';
+// import SharedLayout from './SharedLayout/SharedLayout';
 import AdditionalInformation from '../components/MovieInformation/AdditionalInformation/AdditionalInformation';
+import Home from '../pages/Home';
+import SearchFilms from '../pages/SearchFilms';
+import FilmDetails from '../pages/FilmDetails';
+import NotFound from '../pages/NotFound';
 
-const Home = lazy(() => import('pages/Home'));
-const SearchFilms = lazy(() => import('pages/SearchFilms'));
-const FilmDetails = lazy(() => import('pages/FilmDetails'));
-const NotFound = lazy(() => import('pages/NotFound'));
+// const Home = lazy(() => import('pages/Home'));
+// const SearchFilms = lazy(() => import('pages/SearchFilms'));
+// const FilmDetails = lazy(() => import('pages/FilmDetails'));
+// const NotFound = lazy(() => import('pages/NotFound'));
 
 export const App = () => {
   return (
     <>
+      <Searchbar>
+        <Nav>
+          <LogotypeWrapper>
+            <Logotype />
+            <LogotypeText>Cloud of Films</LogotypeText>
+          </LogotypeWrapper>
+          <NavList>
+            <NavItem>
+              <Link to="/" end>
+                Home
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/search">Search Film</Link>
+            </NavItem>
+          </NavList>
+        </Nav>
+      </Searchbar>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/search" element={<SearchFilms />} />
-          <Route path="/search/:id" element={<FilmDetails />}>
-            <Route path="more" element={<AdditionalInformation />} />
-          </Route>
-          <Route path="*" component={<NotFound />} />
+        {/* <Route path="/" element={<SharedLayout />}> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchFilms />} />
+        <Route path="/search/:id" element={<FilmDetails />}>
+          <Route path="more" element={<AdditionalInformation />} />
         </Route>
+        <Route path="*" component={<NotFound />} />
+        {/* </Route> */}
       </Routes>
     </>
   );
