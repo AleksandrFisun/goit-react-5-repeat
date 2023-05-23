dbb15c8c87c64af5b09230694b3a9eec4590f6e3
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2023-05-23 12:59:51 +0300

    updates

--- src/pages/SearchFilms.jsx
+++ src/pages/SearchFilms.jsx
@@ -1,7 +1,8 @@
-
+import { useLocation } from 'react-router-dom';
 import SearchFilm from '../components/SearchFilm/Search.film';
 const SearchFilms = () => {
-
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
       <SearchFilm />
